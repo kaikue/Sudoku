7b0a77f82eb4c751340866ac109569fffd85385a
rcoker37 <rcoker37@gmail.com>
2018-02-03 19:41:26 -0500

    Setup basic board/squares for sudoku

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject squarePrefab;
+	public GameObject board;
+	public Camera camera;
+	public float squareSeparationX;
+	public float squareSeparationY;
+
+	private GameObject[] squares;
+
+	// Use this for initialization
+	void Start () {
+		InitializeSquares ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 mousePosition = camera.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (new Vector2(mousePosition.x, mousePosition.y), Vector2.zero);
+			GameObject hitSquare = hit.collider.gameObject;
+			hitSquare.GetComponent<SquareController> ().cleared = !hitSquare.GetComponent<SquareController> ().cleared;
+		}
+	}
+
+	void InitializeSquares() {
+		squares = new GameObject[81];
+
+		Vector3 position = squarePrefab.transform.localPosition;
+		for (int y = 0; y < 9; y++) {
+			position.x = squarePrefab.transform.localPosition.x;
+			for (int x = 0; x < 9; x++) {
+				GameObject square = Instantiate (squarePrefab);
+				square.transform.SetParent (board.transform);
+				square.transform.localPosition = position;
+				squares [9 * y + x] = square;
+
+				position.x += squareSeparationX;	
+			}
+			position.y -= squareSeparationY;
+		}
+
+		squarePrefab.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/SquareController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SudokuNumber {
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE
+}
+
+public class SquareController : MonoBehaviour {
+	public SudokuNumber number;
+	public bool cleared;
+
+	private SpriteRenderer sr;
+	private Sprite[] numberSprites;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+		numberSprites = new Sprite[9];
+		LoadNumberSprites ();
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		if (cleared) {
+			sr.sprite = null;
+			//Debug.Log ("Cleared");
+		} else {
+			if (sr.sprite == null) {
+    			//Debug.Log ("Setting sprite");
+				sr.sprite = numberSprites [(int)number];
+			}
+		}
+	}
+
+
+	private void LoadNumberSprites() {
+		for (SudokuNumber number = SudokuNumber.ONE; number <= SudokuNumber.NINE; number++) {
+			string path = "number_" + number.ToString () + "_temp";
+			numberSprites [(int)number] = Resources.Load<Sprite> (path);
+			//Debug.Log (path);
+		}
+	}
+}
