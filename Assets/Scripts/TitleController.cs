4b57dd8914c03509491683270a58732cc2e2dbe2
rcoker37 <rcoker37@gmail.com>
2018-02-12 23:14:34 -0500

    Fixed merge conflict

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour {
+
+	public KeyCode continueKey;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (continueKey)) {
+			// TODO: Go to next scen
+		}
+	}
+}
